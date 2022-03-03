73411d030d922c22f27b315d70c5a6ce9f60f00c
AzkaDzaki <azkadzakiarrazak@gmail.com>
2022-03-03 15:56:38 +0700

    fix: styling button

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ const About = () => {
                 <p className="about__small mb-5">Tools</p>
                 <div className="flex">
                 <a href="#" className="cursor-pointer" onClick={() => {goTo('www.figma.com/file/DEnWUXmtE7mVIbIcAFweZr/SHORTLINK?node-id=0%3A1')}}><img src={Figma} style={{height: '48px', width: '48px'}} className="App-Home mr-6" alt="logo" /></a>
-                <a href="#" className="cursor-pointer" onClick={() => {goTo('www.linkedin.com/in/azka-dzaki-arrazzaq/')}}><img src={ReactImg} style={{height: '48px', width: '55px'}} className="App-Home" alt="logo" /></a>
+                <a href="#" className="cursor-pointer" onClick={() => {goTo('github.com/AzkaDzaki/SHORTLINK')}}><img src={ReactImg} style={{height: '48px', width: '55px'}} className="App-Home" alt="logo" /></a>
                 </div>
             </div>    
             <div>
